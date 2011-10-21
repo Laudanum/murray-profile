4be5cd9c042f094bb84f9fc2413d26d8fa523cd8
Venus Kim <Venus@Venus-Kims-Mac-mini.local>
2011-10-21 02:56:01 -0400

    uploading theme and module

---
+++ sites/all/themes/murray/templates/block--nice-menus--1.tpl.php
@@ -0,0 +1,18 @@
+<?php
+// $Id: gigya-login-block.tpl.php,v 1.1 2010/08/04 14:52:05 add1sun Exp $
+
+/**
+ * @file gigya-login-block.tpl.php
+ * Default theme implementation for displaying a gigya login form.
+ *
+ * Available variables:
+ * - $login_div: Contents of the login block.
+ * - $title: Title to display, if not suppressed.
+ *
+ * @see template_preprocess_gigya_login_block()
+ */
+?>
+ 
+<div id="primary-links" class="menu primary container_12">
+<?php print $content; ?>
+</div>
